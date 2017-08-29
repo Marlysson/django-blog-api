1d04b3352b5a4fea709d9ea5f02e95c740e96019
Marlysson <marlysson5@gmail.com>
2017-08-28 21:34:03 -0300

    Adding handler to insert geo , address and user data to your models.

--- blog/management/commands/populate_blog.py
+++ blog/management/commands/populate_blog.py
@@ -1,6 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
 from dj_blog.settings import BASE_DIR
 
+from blog.models import Geo, Address, User, Post, Comment
+
 import os
 import json
 
@@ -32,7 +34,40 @@ class Command(BaseCommand):
 
         content = self._load_file(file_path)
 
-        print(content)
+        users = content.get("users")
+        posts = content.get("posts")
+        comments = content.get("comments")
+
+        for user in users:
+
+            geo_data = {
+                "latitude": user.get("address").get("geo").get("lat"),
+                "longitude": user.get("address").get("geo").get("lng")
+            }
+
+            address_data = {
+                "street": user.get("address").get("street"),
+                "suite": user.get("address").get("suite"),
+                "city": user.get("address").get("city"),
+                "zipcode": user.get("address").get("zipcode")
+            }
+
+            user_data = {
+                "name": user.get("name"),
+                "email": user.get("email"),
+            }
+
+            geo_object = self._handle_geo(geo_data)
+
+            address_object = self._handle_address(address_data, geo_object)
+
+            user_object = self._handle_users(user_data, address_object)
+
+        for post in posts:
+            pass
+
+        for comment in comments:
+            pass
 
     def _load_file(self,path):
         file_json = open(path)
@@ -43,8 +78,23 @@ class Command(BaseCommand):
     def _verify_json_file(self,json_file):
         return os.path.lexists(json_file) and os.path.isfile(json_file)
 
-    def _handle_users(self,data):
+    def _handle_posts(self,data_object, dependence):
         pass
 
-    def _handle_posts(self,data):
+    def _handle_comments(self,data_object, dependence):
         pass
+
+    def _handle_users(self, data_object, dependence):
+        
+        data_object["address"] = dependence
+
+        return User.objects.create(**data_object)
+
+    def _handle_address(self, data_object, dependence):
+        
+        data_object["geo"] = dependence
+
+        return Address.objects.create(**data_object)
+
+    def _handle_geo(self, data_object):
+        return Geo.objects.create(**data_object)
