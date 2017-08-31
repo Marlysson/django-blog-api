da1840a54bd03f9f3ed87df4ff3b17d379344848
Marlysson <marlysson5@gmail.com>
2017-08-31 02:41:14 -0300

    QUESTION G - Change the QUESTION F to show unique post with the name's user and the content of the comments of this user.

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,47 +4,47 @@ from rest_framework import serializers
 
 class GeoSerializer(serializers.HyperlinkedModelSerializer):
 
-	class Meta:
-		model = Geo
-		fields = ('pk', 'latitude', 'longitude')
+    class Meta:
+        model = Geo
+        fields = ('pk', 'latitude', 'longitude')
 
 
 class AddressSerializer(serializers.HyperlinkedModelSerializer):
 
-	geo = GeoSerializer()
+    geo = GeoSerializer()
 
-	class Meta:
-		model = Address
-		fields = ('pk', 'street', 'suite', 'city', 'zipcode', 'geo')
+    class Meta:
+        model = Address
+        fields = ('pk', 'street', 'suite', 'city', 'zipcode', 'geo')
 
 
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
 
-	class Meta:
-		model = Comment
-		fields = ('url', 'pk', 'name', 'email', 'body')
+    class Meta:
+        model = Comment
+        fields = ('url', 'pk', 'name', 'email', 'body')
 
 
 class PostSerializer(serializers.HyperlinkedModelSerializer):
 
-	user = serializers.SlugRelatedField(queryset=User.objects.all(),slug_field="name")
+    user = serializers.SlugRelatedField(queryset=User.objects.all(),slug_field="name")
 
-	comments = serializers.HyperlinkedRelatedField(many=True,
-												   read_only=True,
-												   view_name="comment-detail")
+    comments = serializers.SlugRelatedField(queryset=Comment.objects.all(),
+                                            slug_field="body",
+                                            many=True)
 
-	class Meta:
-		model = Post
-		fields = ('url', 'pk', 'title','body', 'user', 'comments')
+    class Meta:
+        model = Post
+        fields = ('url', 'pk', 'title','body', 'user', 'comments')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
 
-	address = AddressSerializer()
+    address = AddressSerializer()
 
-	posts = serializers.HyperlinkedRelatedField(many=True,
-												read_only=True,
-												view_name="post-detail")
+    posts = serializers.HyperlinkedRelatedField(many=True,
+                                                read_only=True,
+                                                view_name="post-detail")
 
-	class Meta:
-		model = User
-		fields = ('url', 'pk', 'name', 'email', 'address', 'posts')
+    class Meta:
+        model = User
+        fields = ('url', 'pk', 'name', 'email', 'address', 'posts')
